ef806609fa2f2cbf914c4c3d77a40c8d04303326
DjSaMiX <imdjsamix@gmail.com>
2026-01-08 16:42:28 +0200

    Initial commit (clean Vite + Chakra v2 setup)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Button, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={6}>
+      <Heading mb={4}>Chakra v2 is working ✅</Heading>
+      <Button colorScheme="teal">Test Button</Button>
+    </Box>
+  );
+}
+
+export default App;
